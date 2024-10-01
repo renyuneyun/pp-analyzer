4888181b8a765a49ea8dcd32036826fa7e595f09
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2024-10-01 23:46:38 +0100

    fine-tune: New job for party recognition

--- fine-tune/notebooks/job_preset.py
+++ fine-tune/notebooks/job_preset.py
@@ -255,6 +255,12 @@ _job_presets = [
         training_data_splitter=std.better_split_equal,
     ),
     JobPreset(
+        desc="party-sent-v3-d4",
+        load_data=partial(a_utils.load_and_get, a_utils.get_party_entities_of_sentences_v2),
+        as_training_data=m_utils.as_training_data_for_party_entity_of_sentence_v2,
+        training_data_splitter=f_d4,
+    ),
+    JobPreset(
         desc="relation-seg-v2",
         load_data=partial(a_utils.load_and_get, a_utils.get_relations_of_segment_sentences_no_subsume),
         as_training_data=m_utils.as_training_data_for_relation_of_segment,
