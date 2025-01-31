e2e8aecdcf84e04d7d7b1b8e94b06a3665831fdb
Rui Zhao (renyuneyun) <renyuneyun@gmail.com>
2025-01-31 15:59:43 +0000

    pp-analyze: Add more statistics for analysis

--- pp-analyze/pp_analyze/website_compliance_evaluation.py
+++ pp-analyze/pp_analyze/website_compliance_evaluation.py
@@ -6,10 +6,11 @@ from rdflib import Graph, RDF
 from tqdm.auto import tqdm
 import pp_analyze
 from pp_analyze import user_preference_analyze as upa
-from pp_analyze.data_model import DataEntity, PurposeEntity
+from pp_analyze.data_model import DataEntity, PurposeEntity, SegmentedDataPractice
 from .dtou import NS_DTOU
 from .kg import NS
 from .pp_analyze import get_relative_file_path_for_pp
+from . import policy_text_utils as ptu
 
 
 def get_pesonas_under_dir(persona_base_dir) -> list[str]:
@@ -29,16 +30,30 @@ def get_number_of_conflicts(graph: Graph) -> int:
     return len(list(graph.subjects(RDF.type, NS_DTOU['Conflict'])))
 
 
-def get_number_of_conflicting_segments(graph: Graph) -> int:
+def get_conflicting_segments(graph: Graph) -> set[str]:
     '''
     Get the number of conflicting segments in the graph.
+    If a segment has multiple conflicts, it is counted only once (contrary to above get_number_of_conflicts() which does not take this into account).
     '''
     n_conflict = list(graph.subjects(RDF.type, NS_DTOU['Conflict']))
     distinct_segments = set()
     for node in n_conflict:
         text = graph.value(node, NS['text']).toPython()
         distinct_segments.add(text)
-    return len(distinct_segments)
+    return distinct_segments
+
+
+def get_number_of_conflicting_segments(graph: Graph) -> int:
+    return len(get_conflicting_segments(graph))
+
+
+def get_number_of_conflicting_practices(graph: Graph, practices: list[SegmentedDataPractice]) -> int:
+    conflicting_segments = get_conflicting_segments(graph)
+    num = 0
+    for practice in practices:
+        if practice.segment in conflicting_segments:
+            num += 1
+    return num
 
 
 async def analyze_personas(personas, practices, max_concurrency=5) -> tuple[dict[str, dict[str, Graph]], dict[str, dict[str, str]]]:
@@ -109,6 +124,8 @@ async def analyze_personas(personas, practices, max_concurrency=5) -> tuple[dict
 def to_websites_by_num_conflicts(conflicts, websites_of_interest) -> dict[int, list[str]]:
     '''
     Convert the conflicts dictionary to a dictionary of websites by number of conflicts (as key).
+    "Conflict" here means the number of conflicting personal profiles.
+    This indicates which websites have the same number of conflicts.
     '''
     num_persona_conflicts_per_website = {}
     for ws in websites_of_interest:
@@ -128,7 +145,9 @@ def to_websites_by_num_conflicts(conflicts, websites_of_interest) -> dict[int, l
 
 def to_personas_by_num_conflicts(conflicts, personas, websites_of_interest=None) -> dict[int, list[str]]:
     '''
-    Convert the conflicts dictionary to a dictionary of websites by number of conflicts (as key).
+    Convert the conflicts dictionary to a dictionary of personas by number of conflicts (as key).
+    "Conflict" here means the number of conflicting personal profiles.
+    This indicates which persona has the same number of conflicting websites.
     '''
     num_conflicts_per_persona = {}
     for persona in personas:
@@ -148,6 +167,115 @@ def to_personas_by_num_conflicts(conflicts, personas, websites_of_interest=None)
     personas_by_conflicts = dict(personas_by_conflicts)
 
     return personas_by_conflicts
+
+
+def get_segment_conflict_info(conflicts, websites_of_interest, practices: dict[str, list[SegmentedDataPractice]]) -> dict[str, tuple[int, int, int, int]]:
+    '''
+    Compute several information about the number of conflicting profiles to the number of segments.
+    Returns a dictionary, key is websites, and value is a tuple of target information:
+    - Number of conflicting profiles
+    - Number of segments (that create conflicts, sum over all conflict)
+    - Number of segments (that create conflicts, distinct)
+    - Number of segments (has data practices)
+    - Number of data practices
+    - Number of segments (in privacy policy)
+    - Number of conflicts
+    '''
+    nums_dict = defaultdict(lambda: [0, 0, 0, 0, 0, 0, 0])
+
+    for ws in websites_of_interest:
+        fp = get_relative_file_path_for_pp(ws)
+        n_segments = len(ptu.convert_into_segments(fp.read_text()))
+        nums_dict[ws][5] = n_segments
+        practice_of_website = practices[ws]
+        nums_dict[ws][4] = len(practice_of_website)
+        distinct_segments = set()
+        for practice in practice_of_website:
+            distinct_segments.add(practice.segment)
+        nums_dict[ws][3] = len(distinct_segments)
+
+    conflicting_segments_by_ws = {}
+    for persona, results in conflicts.items():
+        for ws, res in results.items():
+            if ws in websites_of_interest:
+                nums_dict[ws][0] += 1
+                n_segments_conflict = get_number_of_conflicting_segments(res)
+                nums_dict[ws][1] += n_segments_conflict
+                conflicting_segments = get_conflicting_segments(res)
+                if ws not in conflicting_segments_by_ws:
+                    conflicting_segments_by_ws[ws] = set()
+                conflicting_segments_by_ws[ws].update(conflicting_segments)
+
+                number_of_conflicts = get_number_of_conflicts(res)
+                nums_dict[ws][6] += number_of_conflicts
+
+    for ws, segments in conflicting_segments_by_ws.items():
+        nums_dict[ws][2] = len(segments)
+
+    return dict(nums_dict)
+
+
+def calc_average_conflict_rate_by_segment_of_websites(conflicts, websites_of_interest, segment_mode: int = 0, practices: dict[str, list[SegmentedDataPractice]]|None =None) -> dict[int, list[str]]:
+    '''
+    Calculate the average conflict rate of each website, in terms of the number of conflicting profiles per segment.
+    In other words, it is the ratio of the number of conflicting profiles to the number of segments (as the denominator).
+    The segment_mode parameter determines how to count the number of segments, as follows:
+    - 0: Count the number of all segments in the privacy policy.
+    - 1: Count the number of segments that create conflicts (for each profile; if same segment creates multiple conflicting profiles, it is counted multiple times).
+        This calculates on average how many conflicting profiles are created per conflicting segment.
+    - 2: Same as 1, but do not count the same segment multiple times.
+    - 3: Count the number of segments that has data practices (requires `practices` parameter).
+        This estimates the average number of conflicting profiles each valid segment creates.
+    - 4: Same as 3, but do not count the same segment multiple times.
+    - 5: Same as 0, but the numerator is 1 if there is a conflict, 0 otherwise.
+    '''
+    num_persona_conflicts_per_website = {}
+    for ws in websites_of_interest:
+        num_persona_conflicts_per_website[ws] = 0
+    for persona, results in conflicts.items():
+        for ws, res in results.items():
+            if ws in websites_of_interest:
+                if segment_mode == 5:
+                    num_persona_conflicts_per_website[ws] = 1
+                else:
+                    num_persona_conflicts_per_website[ws] += 1
+
+    ret = {}
+    for ws, num_conflicts in num_persona_conflicts_per_website.items():
+        if segment_mode == 0 or segment_mode == 5:
+            fp = get_relative_file_path_for_pp(ws)
+            n_segments = len(ptu.convert_into_segments(fp.read_text()))
+        elif segment_mode == 1:
+            n_segments = 0
+            for persona, results in conflicts.items():
+                if ws in results:
+                    n_segments += get_number_of_conflicting_segments(results[ws])
+        elif segment_mode == 2:
+            conflicting_segments = set()
+            for persona, results in conflicts.items():
+                if ws in results:
+                    conflicting_segments.update(get_conflicting_segments(results[ws]))
+            n_segments = len(conflicting_segments)
+        elif segment_mode == 3:
+            practice_of_website = practices[ws]
+            n_segments = 0
+            for practice in practice_of_website:
+                n_segments += 1
+        elif segment_mode == 4:
+            practice_of_website = practices[ws]
+            valid_segments = set()
+            for practice in practice_of_website:
+                valid_segments.add(practice.segment)
+            n_segments = len(valid_segments)
+        else:
+            raise ValueError("Invalid segment_mode")
+        if num_conflicts == 0:
+            ret[ws] = 0
+        else:
+            ret[ws] = num_conflicts / n_segments
+
+    return ret
+
 
 def websites_with_same_pp(website_list):
     pp_dict = {}
